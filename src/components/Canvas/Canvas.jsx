cf26074aed52fda2219cb805abc8abe42f95ce00
Novik <yaroslavnowik@yandex.ru>
2024-02-13 23:24:45 +0100

    animation lib added

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -1,12 +1,40 @@
+import { motion } from 'framer-motion';
 import Selection from '../Selection/Selection';
 import './canvas.css';
 import picture from '../../img/picture.png';
+import { useState } from 'react';
 
 export default function Canvas() {
+  const [animated, setAnimated] = useState(false);
   return (
     <div className='canvas'>
       <div className='canvas__container-left'>
-        <h1 className='canvas__title'>Animation Settings</h1>
+        <motion.h1
+          initial={{
+            x: 160,
+            y: 160,
+            opacity: 0,
+            scale: 1.5,
+          }}
+          animate={
+            animated
+              ? {
+                  x: 0,
+                  y: 0,
+                  opacity: 1,
+                  scale: 1,
+                  transition: {
+                    duration: 3,
+                    delay: 0.2,
+                    ease: 'easeInOut',
+                  },
+                }
+              : {}
+          }
+          className='canvas__title'
+        >
+          Animation Settings
+        </motion.h1>
         <p className='canvas__text'>
           The user should have the option to select any element on the page and
           set up its animation using the controls in the right panel. A dotted
@@ -15,7 +43,14 @@ export default function Canvas() {
           The preview button on the top panel will open the result in a new tab.
         </p>
         <Selection>
-          <button className='canvas__button'>Button</button>
+          <button
+            className='canvas__button'
+            onClick={() => {
+              setAnimated(!animated);
+            }}
+          >
+            Button
+          </button>
         </Selection>
       </div>
       <div className='canvas__container-right'>
